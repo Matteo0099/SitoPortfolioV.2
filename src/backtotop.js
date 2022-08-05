fd1d1c0676ed789cc5a629f7bcae12a63f71242c
Matty <74145047+Matteo0099@users.noreply.github.com>
2022-08-05 23:56:21 +0200

    modifiche ai titoli

--- src/backtotop.js
+++ src/backtotop.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 2500) {
-        $('.move-top').fadeIn();
-      } else {
-        $('.move-top').fadeOut();
-      }
-    });
-  
-    $('.move-top').click(function() {
-      $("html, body").animate({
-        scrollTop: 0
-      }, 600);
-      return false;
-    });
+  var divsToHide = document.getElementsByClassName("move-top");
+  for(var i = 0; i < divsToHide.length; i++) {
+    divsToHide[i].getElementsByClassName.display = "none";
+  }
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() <= 2900) {
+      $('.move-top').fadeOut();
+      
+    } else {
+      console.log('Hide');
+      $('.move-top').fadeIn();
+    }
+    if ($(this).scrollTop() == 0 || $(this).scrollTop() >= 0 && $(this).scrollTop() < 2800 || $(this).scrollTop() <= 0) {
+      $('.move-top').fadeOut();
+    }
+  });
+
+  $('.move-top').click(function() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 600);
+    return false;
+  });
 });
